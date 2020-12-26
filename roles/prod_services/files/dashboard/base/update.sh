3b349800b7f844620b7a55dbc4e970ec73bbf286
David Snider <david@davidsnider.org>
2020-12-26 10:10:31 -0700

    update kustomize and dashboard

--- roles/prod_services/files/dashboard/base/update.sh
+++ roles/prod_services/files/dashboard/base/update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm recommended.yaml
-wget https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.5/aio/deploy/recommended.yaml
+wget https://raw.githubusercontent.com/kubernetes/dashboard/v2.1.0/aio/deploy/recommended.yaml
